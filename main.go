d9bbab939517070097a8ff85060cf09a71bb7e5c
alfi sahri <alfisahri33@gmail.com>
2022-11-30 22:58:10 +0700

    Add Dockerfile

--- main.go
+++ main.go
@@ -4,18 +4,24 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	router := mux.NewRouter()
+
+	if port == "" {
+		port = "5000"
+	}
 
 	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{"message": "Hello world"})
 	})
 
-	log.Println("API is running on port 5000!")
-	http.ListenAndServe(":5000", router)
+	log.Println("API is running on port 5000")
+	http.ListenAndServe(":"+port, router)
 }
