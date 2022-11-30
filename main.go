78259427c6cd1966ba92ab661dc5fe885ba781ac
alfi sahri <alfisahri33@gmail.com>
2022-11-30 21:39:45 +0700

    init hello golang starter

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": "Hello world"})
+	})
+
+	log.Println("API is running on port 5000!")
+	http.ListenAndServe(":5000", router)
+}
