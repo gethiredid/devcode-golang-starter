02bba98bb79989a2505f2dc047551fd5318437ba
alfi sahri <alfisahri33@gmail.com>
2022-12-07 14:27:48 +0700

    add env

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module devcode-golang-starter
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+)
--- main.go
+++ main.go
@@ -7,21 +7,33 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	router := mux.NewRouter()
-
-	if port == "" {
-		port = "3030"
+	// load env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
+
+	// get env
+	PORT := getEnv("PORT", "3030")
+	router := mux.NewRouter()
 
 	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{"message": "Hello world"})
 	})
 
-	log.Println("API is running on port 3030")
-	http.ListenAndServe(":"+port, router)
+	log.Println("API is running on port " + PORT)
+	http.ListenAndServe(":"+PORT, router)
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) > 0 {
+		return value
+	}
+	return defaultValue
 }
